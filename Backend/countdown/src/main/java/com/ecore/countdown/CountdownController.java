28629b003ac6588ba866997e93f486d0d95dd562
Amanda Valle <amandabrvalle@gmail.com>
2023-02-09 09:24:57 -0300

    cleaning DB operations

--- Backend/countdown/src/main/java/com/ecore/countdown/CountdownController.java
+++ Backend/countdown/src/main/java/com/ecore/countdown/CountdownController.java
@@ -19,24 +19,10 @@ public class CountdownController {
         this.eventRepository = eventRepository;
     }
 
-    @GetMapping("/countdown/events")
-    public String getTimeRemaining(@RequestParam("targetDateTime") String targetDateTime) {
-        LocalDateTime target = LocalDateTime.parse(targetDateTime, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-        LocalDateTime now = LocalDateTime.now();
-        Duration duration = Duration.between(now, target);
-        long seconds = duration.getSeconds();
-        long minutes = seconds / 60;
-        long hours = minutes / 60;
-        long days = hours / 24;
-        return String.format("%d days, %d hours, %d minutes, and %d seconds", days, hours % 24, minutes % 60, seconds % 60);
-    }
-
     @PostMapping("/countdown/events")
     public @ResponseBody String calculateTime(@RequestBody Event event) {
-        Event savedEvent = eventRepository.save(event);
         String timeDiff = new Gson().toJson(event.getTimeRemaining(event.getDate()));
         System.out.println(timeDiff);
-        //System.out.println(eventRepository.findAllById(event.getId()));
         return timeDiff;
     }
 }
--- Backend/countdown/src/main/java/com/ecore/countdown/DB/EventRepository.java
+++ Backend/countdown/src/main/java/com/ecore/countdown/DB/EventRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface EventRepository extends CrudRepository<Event, Long> {
-    List<Event> findAllById(Long id);
+
 }
